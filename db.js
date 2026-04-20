ef5246f7db0136617ac190acd359651939eab9aa
EbubekirGonan <ebbkrgnn@gmail.com>
2026-04-20 23:29:32 +0300

    modified db.js for live server

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Database = require('better-sqlite3');
 const path = require('path');
 const fs = require('fs');
 
-const dataDir = path.join(__dirname, 'data');
+const dataDir = process.env.NODE_ENV === 'production' ? '/data' : path.join(__dirname, 'data');
 if (!fs.existsSync(dataDir)) fs.mkdirSync(dataDir, { recursive: true });
 
 const db = new Database(path.join(dataDir, 'todap.db'));
